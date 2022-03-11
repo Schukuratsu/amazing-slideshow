614b671d29e6936dd74c87a6518fdcf2b671a66e
Lucas Ennes <lucas.ennes@upperds.com>
2022-03-10 21:50:54 -0300

    adding required props, showing frames on correct time and implementing autostop

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
-import './App.css';
-import { FramePlayer } from './components/FramePlayer';
+import React from "react";
+import "./App.css";
+import { FramePlayer } from "./components/FramePlayer";
+
+const FRAMES = [
+  "/image0.jpg",
+  "/image1.jpg",
+  "/image2.jpg",
+  "/image3.jpg",
+  "/image4.jpg",
+];
 
 function App() {
   return (
     <div className="App">
-      <FramePlayer />
+      <FramePlayer frames={FRAMES} fps={1} />
     </div>
   );
 }
--- src/components/FramePlayer/index.tsx
+++ src/components/FramePlayer/index.tsx
@@ -1,26 +1,47 @@
 import { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 
+type Props = {
+  frames: string[];
+  fps: number;
+};
+
 const INTERVAL_IN_MILISECONDS = 10;
 
-export function FramePlayer() {
+export function FramePlayer({ frames, fps }: Props) {
   const [currentTimeInMiliseconds, setCurrentTimeInMiliseconds] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
 
+  const totalTimeInMiliseconds = (1000 * frames.length) / fps;
+
+  /* using separate useEffects for the time increments and autostop because 
+  a big delay happens when the "setInterval" needs to be called on every tick. */
   useEffect(() => {
     const intervalId = setInterval(() => {
       if (isPlaying) {
         setCurrentTimeInMiliseconds((value) => value + INTERVAL_IN_MILISECONDS);
       }
     }, INTERVAL_IN_MILISECONDS);
-
     return () => clearInterval(intervalId);
   }, [isPlaying]);
 
+  useEffect(() => {
+    if (currentTimeInMiliseconds >= totalTimeInMiliseconds) {
+      setIsPlaying(false);
+      setCurrentTimeInMiliseconds(totalTimeInMiliseconds);
+    }
+  }, [currentTimeInMiliseconds, totalTimeInMiliseconds]);
+
+  const currentFrame = Math.floor((fps * currentTimeInMiliseconds) / 1000);
+
   return (
     <div className={styles.framePlayer}>
+      <img
+        src={frames[currentFrame] ?? frames.at(-1)}
+        alt="current frame"
+        className={styles.frame}
+      />
       <div>{currentTimeInMiliseconds.toString()}</div>
-      <div></div>
       <div>
         <button onClick={() => setIsPlaying((value) => !value)}>
           {isPlaying ? "Pause" : "Play"}
